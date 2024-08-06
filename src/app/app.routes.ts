19dabef228bfeaf9fad54890be95e6be6cd32845
EliAlarcon <EliAlarcon@users.noreply.github.com>
2024-08-06 12:20:32 -0500

    Componente course e imagenes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { CourseComponent } from './pages/course/course.component';
 
 export const routes: Routes = [
-    { path: '', component: LoginComponent}
+    { path: '', component: LoginComponent},
+    { path: 'curso', component: CourseComponent},
 ];
