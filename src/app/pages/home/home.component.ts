db6272d32f31efd4564d8f1e77c59f882e9bdd17
DayannaMacay <dayanamacay@hotmail.com>
2024-08-06 17:59:12 -0500

    Navbar Top

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from 'src/app/components/footer/footer.component';
+import { NavBarTopComponent } from 'src/app/components/nav-bar-top/nav-bar-top.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, NavBarTopComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
