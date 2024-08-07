94478c3b90b4084078cae7a0626917194f5115c2
DenisChasipanta <DenisChasipanta@users.noreply.github.com>
2024-08-06 21:26:02 -0500

    Creación de ventana para ver las notas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CourseComponent } from './pages/course/course.component';
+import { GradesComponent } from './pages/grades/grades.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent},
     { path: 'home', component: HomeComponent},
     { path: 'curso', component: CourseComponent},
+    { path: 'grades', component: GradesComponent},
 ];
