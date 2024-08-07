5c8895acf13c1299e39cf72c8e98cbb827cccecf
DayannaMacay <dayanamacay@hotmail.com>
2024-08-06 19:35:01 -0500

    Ventana perfil

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from 'src/app/components/footer/footer.component';
+import { NavBarTopComponent } from 'src/app/components/nav-bar-top/nav-bar-top.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [NavBarTopComponent, FooterComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
