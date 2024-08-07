e1cac9ce0fbb1775cd98bc8ec61b15bbae48734f
EliAlarcon <EliAlarcon@users.noreply.github.com>
2024-08-06 19:50:29 -0500

    Cambios logo

--- src/app/pages/course/course.component.ts
+++ src/app/pages/course/course.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
+import { NavBarTopComponent } from "../../components/nav-bar-top/nav-bar-top.component";
 
 @Component({
   selector: 'app-course',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, NavBarTopComponent],
   templateUrl: './course.component.html',
   styleUrl: './course.component.css'
 })
