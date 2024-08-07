f9e9f6535faad03f6d0adce3f0a04f40ef3b9ea4
EliAlarcon <EliAlarcon@users.noreply.github.com>
2024-08-06 19:22:59 -0500

    Cabio Ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { CourseComponent } from './pages/course/course.component';
 
 export const routes: Routes = [
-    { path: '', component: LoginComponent},
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    { path: 'login', component: LoginComponent},
     { path: 'home', component: HomeComponent},
     { path: 'curso', component: CourseComponent},
 ];
