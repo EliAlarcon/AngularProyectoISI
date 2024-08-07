c70db529b30c9a580aca7651544c66cfef100689
EliAlarcon <EliAlarcon@users.noreply.github.com>
2024-08-06 22:56:09 -0500

    Componente Grades integrado

--- src/app/pages/grades/grades.component.ts
+++ src/app/pages/grades/grades.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
 import { Router } from '@angular/router';
+import { NavBarTopComponent } from "../../components/nav-bar-top/nav-bar-top.component";
 
 @Component({
   selector: 'app-grades',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, NavBarTopComponent],
   templateUrl: './grades.component.html',
   styleUrl: './grades.component.css'
 })
