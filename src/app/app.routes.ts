43998fb4fee68b84480d7c54076ab8e03ff83bea
Anthony Luzon <anthonyluzon124@gmail.com>
2024-08-11 16:59:54 -0500

    Cambios propios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CourseComponent } from './pages/course/course.component';
+import { HomeworkComponent } from './pages/homework/homework.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent},
     { path: 'home', component: HomeComponent},
     { path: 'curso', component: CourseComponent},
+    { path: 'tarea', component: HomeworkComponent},
 ];
---
+++ src/app/pages/homework/homework.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from 'src/app/components/nav-bar/nav-bar.component';
+
+@Component({
+  selector: 'app-homework',
+  standalone: true,
+  imports: [NavBarComponent],
+  templateUrl: './homework.component.html',
+  styleUrl: './homework.component.css'
+})
+export class HomeworkComponent {
+
+}
